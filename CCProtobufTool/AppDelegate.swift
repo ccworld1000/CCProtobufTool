0d663e5eab5d63a5a8effe0687a3f4005d359f07
CC (deng you hua) <ccworld1000@gmail.com>
2019-10-30 17:25:36 +0800

    CCAutoTag push : init

---
+++ CCProtobufTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CCProtobufTool
+//
+//  Created by CC on 2019/10/30.
+//  Copyright © 2019 CC (deng you hua | cworld1000@gmail.com). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CCProtobufTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CCProtobufTool
+//
+//  Created by CC on 2019/10/30.
+//  Copyright © 2019 CC (deng you hua | cworld1000@gmail.com). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
